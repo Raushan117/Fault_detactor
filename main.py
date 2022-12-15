775cea571619b921adef3b573453dbd53c4cfbae
Raushan <raushankumar9060472gamil.com>
2022-12-15 13:59:17 +0530

    validation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sensor.utils import get_collection_as_dataframe
 import sys,os
 from sensor.entity import config_entity
 from sensor.component.data_ingestion import DataIngestion
+from sensor.component.data_validation import DataValidation
 
 
 
@@ -11,8 +12,15 @@ if __name__=="__main__":
      try:
           training_pipeline_config = config_entity.TrainingPipelineConfig()
           data_ingestion_config  = config_entity.DataIngestionConfig(traning_pipeline_config=training_pipeline_config)
-          print(data_ingestion_config)
+          print(data_ingestion_config.to_dict())
           data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
           print(data_ingestion.initiate_data_ingestion())
+
+
+          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation = DataValidation(data_validation_config=data_validation_config , data_ingestion_artifact= data_ingestion_artifact)
+
+          data_validation_artifact = data_validation.initiate_data_validation()
      except Exception as e:
           print(e)
